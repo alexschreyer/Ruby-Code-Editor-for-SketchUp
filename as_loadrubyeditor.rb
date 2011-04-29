a3742cefb7c901d6186a2c5dd5e160812278e30a
Alexander C. Schreyer <mail@alexschreyer.net>
2011-04-29 10:38:59 -0400

    Changed version number in loader.

--- as_loadrubyeditor.rb
+++ as_loadrubyeditor.rb
@@ -4,8 +4,8 @@ require 'sketchup.rb'
 require 'extensions.rb'
 
 as_rubyeditor = SketchupExtension.new "Ruby Code Editor", "as_plugins/as_rubyeditor/as_rubyeditor.rb"
-as_rubyeditor.copyright= 'Copyright 2010 Alexander C. Schreyer'
+as_rubyeditor.copyright= 'Copyright 2010-2011 Alexander C. Schreyer, GPL License'
 as_rubyeditor.creator= 'Alexander C. Schreyer, www.alexschreyer.net'
-as_rubyeditor.version = '1.2'
-as_rubyeditor.description = "This code editor allows creating and modifying Ruby scripts within SketchUp. These scripts can be used to create geometry, add functionality or add data within the SketchUp 3D modeling environment."
+as_rubyeditor.version = '2.0'
+as_rubyeditor.description = "This code editor allows creating and modifying Ruby (and other) scripts within SketchUp. These scripts can be used to create geometry, add functionality or add data within the SketchUp 3D modeling environment."
 Sketchup.register_extension as_rubyeditor, true
